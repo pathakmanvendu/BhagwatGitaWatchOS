5d8f2e7e6545790b2edcd233b6a942daa35b98b4
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-04-13 12:50:05 +0530

    Initial Commit

---
+++ BhagwatGeeta Watch App/BhagwatGeetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BhagwatGeetaApp.swift
+//  BhagwatGeeta Watch App
+//
+//  Created by manvendu pathak  on 13/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BhagwatGeeta_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
