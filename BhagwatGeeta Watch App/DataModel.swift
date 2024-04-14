abd4675712675229761cd1f76cb0211aec685cd8
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-04-14 10:39:06 +0530

    First Commit

---
+++ BhagwatGeeta Watch App/DataModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataModel.swift
+//  BhagwatGeeta Watch App
+//
+//  Created by manvendu pathak  on 13/04/24.
+//
+
+import Foundation
---
+++ BhagwatGeeta Watch App/MeaningScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MeaningScreen.swift
+//  BhagwatGeeta Watch App
+//
+//  Created by manvendu pathak  on 13/04/24.
+//
+
+import SwiftUI
+
+struct MeaningScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeaningScreen()
+}
